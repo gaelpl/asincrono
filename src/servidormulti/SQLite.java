8a1caa622493f0f82dbff312d284452d0e16d5d0
gael <gaelpacheco@gmail.com>
2025-10-15 18:52:32 -0700

    se agrego el cargar el driver, si carga bien se crean las tablas con un metodo que todavia no existe

--- src/servidormulti/SQLite.java
+++ src/servidormulti/SQLite.java
@@ -1,5 +1,17 @@
 package servidormulti;
 
 public class SQLite {
+    //url de la base de datos
+    private static final String URL = "jdbc:sqlite:datos.db";
+
+    try {
+        //aqui pido cargar el driver JDBC
+            Class.forName("org.sqlite.JDBC");
+            System.out.println("Driver JDBC de SQLite cargado.");
+            crearTablas(); //metodo todavia no implementado
+        } catch (ClassNotFoundException e) {
+            System.err.println("Error: Driver JDBC de SQLite no encontrado. Asegúrate de tener el JAR en el classpath.");
+        }
     
+
 }
