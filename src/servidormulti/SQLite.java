c9987fd748337ecdbfbe3a5e87724a27b19a263b
gael <gaelpacheco@gmail.com>
2025-10-15 19:23:33 -0700

    agrego metodo para establecer conexion con el archivo de la base de datos  y el metodo para crear las tablas

--- src/servidormulti/SQLite.java
+++ src/servidormulti/SQLite.java
@@ -1,17 +1,40 @@
 package servidormulti;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class SQLite {
     //url de la base de datos
     private static final String URL = "jdbc:sqlite:datos.db";
 
+    public SQLite() {
     try {
         //aqui pido cargar el driver JDBC
             Class.forName("org.sqlite.JDBC");
             System.out.println("Driver JDBC de SQLite cargado.");
-            crearTablas(); //metodo todavia no implementado
+            crearTablas();
         } catch (ClassNotFoundException e) {
             System.err.println("Error: Driver JDBC de SQLite no encontrado. Asegúrate de tener el JAR en el classpath.");
         }
     
+}
 
+private Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
+
+    private void crearTablas() {
+        String sqlUsuarios = "CREATE TABLE IF NOT EXISTS USUARIOS ("
+                + "usuario TEXT PRIMARY KEY NOT NULL,"
+                + "contrasena TEXT NOT NULL"
+                + ");";
+
+        String sqlBloqueos = "CREATE TABLE IF NOT EXISTS BLOQUEOS ("
+                + "bloqueador TEXT NOT NULL,"
+                + "bloqueado TEXT NOT NULL,"
+                + "FOREIGN KEY (bloqueador) REFERENCES USUARIOS(usuario) ON DELETE CASCADE,"
+                + "FOREIGN KEY (bloqueado) REFERENCES USUARIOS(usuario) ON DELETE CASCADE,"
+                + "PRIMARY KEY (bloqueador, bloqueado)"
+                + ");";
+    }
 }
