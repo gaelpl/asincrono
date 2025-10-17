ef8b00c8455602e6b2ae527352704603c7c2658a
gael <gaelpacheco@gmail.com>
2025-10-17 13:02:49 -0700

    metodo para verificar si usuario existe pero ahora en la base de datos

--- src/servidormulti/comandosDAO.java
+++ src/servidormulti/comandosDAO.java
@@ -1,5 +1,24 @@
 package servidormulti;
 
-public class comandosDAO {    
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class comandosDAO {
     
+    private Connection getConnection() throws SQLException {
+        return ServidorMulti.getManejador().conectar();
+    }
+
+    public boolean verificarUsuarioExistente(String usuario) throws SQLException {
+        String sql = "SELECT usuario FROM USUARIOS WHERE usuario = ?";
+        try (Connection conn = getConnection(); 
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            
+            pstmt.setString(1, usuario);
+            ResultSet rs = pstmt.executeQuery();
+            return rs.next(); 
+        }
+    }
 }
